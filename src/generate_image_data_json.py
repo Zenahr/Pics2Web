e0094d0a40b4146c99102f116eee87c0e2c8917d
Zenahr <zenmatica@gmail.com>
2020-12-25 01:37:44 +0100

    add basic flow notes. add filepath algo

--- src/generate_image_data_json.py
+++ src/generate_image_data_json.py
@@ -2,20 +2,51 @@ import os
 from glob import glob1
 import jinja2 as j
 
-DESTINATION='./build'
+CONTENT_FOLDER='.\\content'
+DESTINATION='.\\build'
+GALLERY_WEB_PATH='.\\assets\\gallery'
+
+def generate_imgs_src(content_folder):
+    """Generate web-ready list of full filepaths for jinja2
+
+    Args:
+        content_folder (string): path to content folder
+    """
+
+    result = []
+    for root, dirs, files in os.walk(CONTENT_FOLDER):
+        for filename in files:
+            result.append(os.path.join(GALLERY_WEB_PATH, filename))
+    print(result[0])
+
+
+generate_imgs_src(CONTENT_FOLDER)
+
+
 
 def procedure():
-    file_loader = j.FileSystemLoader('./src/templates')
-    env = j.Environment(loader=file_loader)
 
-    template = env.get_template('index.template.html')
-    stream = template.render(
-        images=[1,2,3,4,5],
-    )
+    """
+    copy contents of CONTENT_FOLDER to GALLERY_WEB_PATH
+    generate web-ready paths to images generate_imgs_src() -> [src] (./assets/gallery/filename.extension)
+    pass [src] back to generator
+    generate HTML and save to DESTINATION
 
-    with open(
-        os.path.join(DESTINATION, 'index.html'), 'w', encoding='utf-8'
-    ) as out:
-        out.write(stream)
+    """
 
-procedure()
+
+
+    # file_loader = j.FileSystemLoader('./src/templates')
+    # env = j.Environment(loader=file_loader)
+
+    # template = env.get_template('index.template.html')
+    # stream = template.render(
+    #     images=[1,2,3,4,5],
+    # )
+
+    # with open(
+    #     os.path.join(DESTINATION, 'index.html'), 'w', encoding='utf-8'
+    # ) as out:
+    #     out.write(stream)
+
+# procedure()
